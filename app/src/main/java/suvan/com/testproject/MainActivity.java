5aa864785234a3e3f84d44042d512dd29d7760bb
jibinjoy2 <jibin.joy2@gmail.com>
2017-02-17 12:02:02 +0530

    my commit message

--- app/src/main/java/suvan/com/testproject/MainActivity.java
+++ app/src/main/java/suvan/com/testproject/MainActivity.java
@@ -2,15 +2,43 @@ package suvan.com.testproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
 public class MainActivity extends AppCompatActivity {
+    Set<String> stringSet;
+    List<String> stringList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        stringList = new ArrayList<>();
+
+        stringList.add("jinson");
+        stringList.add("jinson");
+        stringList.add("jinson");
+        stringList.add("asd");
+        stringList.add("asd");
+        stringList.add("asd");
+
+        stringList.add("zxy");
+        stringList.add("bbb");
+        stringList.add("bbbc");
+
         Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
+
+        stringSet = new LinkedHashSet<>(stringList);
+        stringList.clear();
+        stringList.addAll(stringSet);
+
+        Log.d("ads","");
     }
 }
