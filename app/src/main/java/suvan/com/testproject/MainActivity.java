c6656b589457b8779cd2714fb8e7ccdc99869f1a
Jibin Joy <jibin.joy2@gmail.com>
2017-02-17 11:28:54 +0530

    Update MainActivity.java

--- app/src/main/java/suvan/com/testproject/MainActivity.java
+++ app/src/main/java/suvan/com/testproject/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "Hi", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
     }
 }
